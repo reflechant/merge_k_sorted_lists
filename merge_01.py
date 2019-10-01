a66db4a19fa0d6f3f0b5f3c95ce4f3a05f410428
R.G <reflechant@users.noreply.github.com>
2019-10-01 23:43:32 +0300

    some changes

--- merge_01.py
+++ merge_01.py
@@ -1,24 +1,34 @@
-from typing import Iterator
+def merge(k, lines):
+    data = [bytearray(0)] * k
+    sz = [0] * k
+
+    for i, line in enumerate(lines):
+        sz[i] = line[0]
+        data[i] = bytearray(line[1:])
+
+    idx = [0] * k
+    stop = False
+    while not stop:
+        stop = True
+        local_min = 101
+        local_idx = 0
+        for i in range(k):
+            if idx[i] < sz[i]:
+                stop = False
+                local_min = min(local_min, data[i][idx[i]])
+                local_idx = i
+        idx[local_idx] += 1
+        yield local_min
 
 
-def merge(k: int, lines: Iterator[Iterator[int]]) -> bytearray:
-    array = bytearray(0)
-    if k == 0:
-        return array
-    # line = (int(x) for x in input().split())
-    line = next(lines)
-    for i in range(k):
-        sz = next(line)
-        array = array + bytearray(line)
-
-    return array
+def main():
+    k = int(input())
+    lines = ((int(x) for x in input().split()) for i in range(k))
+    # lines = [[int(x) for x in input().split()] for i in range(k)]
+    result = merge(k, lines)
+    for x in result:
+        print(x, end=' ')
 
 
 if __name__ == '__main__':
-    k = int(input())
-    lines = ((int(x) for x in input().split()) for i in range(k))
-    # lines = list(lines)
-    # print(lines)
-    result = merge(k, lines)
-    if result:
-        print(result)
+    main()
--- test_merge.py
+++ test_merge.py
@@ -2,16 +2,41 @@ from merge_01 import merge
 
 
 def test_zero_lines():
-    assert (merge(0, lines)) == []
+    assert list(merge(0, [])) == []
 
 
 def test_one_line_zero_elements():
-    assert (merge(1, lines)) == []
+    assert list(merge(1, [[0]])) == []
 
 
 def test_one_line_one_zero_element():
-    assert (merge(1, lines)) == [0]
+    assert list(merge(1, [[1, 0]])) == [0]
 
 
 def test_one_line_one_nonzero_element():
-    assert (merge(1, lines)) == [5]
+    assert list(merge(1, [[1, 5]])) == [5]
+
+
+def test_one_line_multiple_elements():
+    assert list(merge(1, [[3, 7, 8, 9]])) == [7, 8, 9]
+    assert list(merge(1, [[3, 0, 0, 0]])) == [0, 0, 0]
+
+
+def test_multiple_lines_of_zero_element():
+    assert list(merge(2, [[0], [0]])) == []
+    assert list(merge(3, [[0], [0], [0]])) == []
+
+
+def test_multiple_lines_of_one_element():
+    assert list(merge(2, [[1, 0], [1, 0]])) == [0, 0]
+    assert list(merge(2, [[1, 4], [1, 5]])) == [4, 5]
+    assert list(merge(2, [[1, 5], [1, 4]])) == [4, 5]
+
+
+def test_generic_case():
+    assert list(merge(4, [[6, 2, 26, 64, 88, 96, 96],
+                          [4, 8, 20, 65, 86],
+                          [7, 1, 4, 16, 42, 58, 61, 69],
+                          [1, 84]])) == [1, 2, 4, 8, 16, 20, 26, 42,
+                                         58, 61, 64, 65, 69, 84, 86,
+                                         88, 96, 96]
