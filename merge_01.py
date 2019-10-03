88dc32cd4b917f042c2a1826e2c328a3445510c7
R.G <reflechant@users.noreply.github.com>
2019-10-03 16:14:23 +0300

    Proper algorithm with heapq. Passes tests.

--- merge_01.py
+++ merge_01.py
@@ -1,33 +1,24 @@
-def merge(k, lines):
-    data = [bytearray(0)] * k
-    sz = [0] * k
+import heapq
 
-    for i, line in enumerate(lines):
-        sz[i] = line[0]
-        data[i] = bytearray(line[1:])
 
-    idx = [0] * k
-    stop = False
-    while not stop:
-        stop = True
-        local_min = 101
-        local_idx = 0
-        for i in range(k):
-            if idx[i] < sz[i]:
-                stop = False
-                local_min = min(local_min, data[i][idx[i]])
-                local_idx = i
-        idx[local_idx] += 1
-        yield local_min
+def merge(k, data):
+    data = (ba[1:] for ba in data)
+    heap = heapq.merge(*data)
+    return heap
+
+
+def read_data(k):
+    for _ in range(k):
+        yield bytearray(int(x) for x in input().split())
 
 
 def main():
     k = int(input())
-    lines = ((int(x) for x in input().split()) for i in range(k))
-    # lines = [[int(x) for x in input().split()] for i in range(k)]
-    result = merge(k, lines)
-    for x in result:
-        print(x, end=' ')
+    result = merge(k, read_data(k))
+    if result:
+        for x in result:
+            print(x, end=' ')
+    print()
 
 
 if __name__ == '__main__':
